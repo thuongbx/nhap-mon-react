c483b1b9315e75af4cb967b064077e5762b4c7d1
Tuyen Pham <tuyen.at.work@gmail.com>
2022-12-05 20:05:18 +0700

    Sua style cho toan trang

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import DanhSachSanPham from './components/danh-sach-san-pham/DanhSachSanPham'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import DanhSachSanPham from "./components/danh-sach-san-pham/DanhSachSanPham";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header/>
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-    <DanhSachSanPham/>
-    <Footer/>
+      <Header />
+      <main>
+        <DanhSachSanPham />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
