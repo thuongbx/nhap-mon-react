6459b9e4ce047d68b5d12ed8323b20b28d077ae0
Tuyen Pham <tuyen.at.work@gmail.com>
2022-12-05 21:00:00 +0700

    Sua duong dan anh

--- src/components/the-san-pham/TheSanPham.tsx
+++ src/components/the-san-pham/TheSanPham.tsx
@@ -1,14 +1,16 @@
-import './TheSanPham.scss';
-import logo from '@assets/Nike Zoom Kd 12.png';
+import "./TheSanPham.scss";
+import logo from "@assets/nike.png";
 
 const TheSanPham = () => {
-    return <div className='the-san-pham'>
-        <img src={logo} alt="" className="the-san-pham__img" />
-        <div className="the-san-pham__list">
-            <h3 className="the-san-pham__list-name">Nike Zoom KD 12</h3>
-            <span className="the-san-pham__list-price">$ 99</span>
-        </div>
+  return (
+    <div className="the-san-pham">
+      <img src={logo} alt="" className="the-san-pham__img" />
+      <div className="the-san-pham__list">
+        <h3 className="the-san-pham__list-name">Nike Zoom KD 12</h3>
+        <span className="the-san-pham__list-price">$ 99</span>
+      </div>
     </div>
-}
+  );
+};
 
 export default TheSanPham;
